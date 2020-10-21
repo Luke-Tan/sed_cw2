dd561e6732a7c4601d62d4c29ad6ad10e104258d
Luke Tan <Luke@Lukes-MacBook-Air.local>
2020-10-21 21:00:13 +0800

    Switching the camera on powers up the sensor

--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -1,18 +1,27 @@
 package ic.doc.camera;
 
+import org.jmock.Expectations;
 import org.jmock.integration.junit4.JUnitRuleMockery;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class CameraTest {
+  private Camera camera;
 
   @Rule
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
+  private Sensor sensor = context.mock(Sensor.class);
+
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
+    camera = new Camera(sensor);
 
-    // write your test here
+    context.checking(new Expectations() {{
+      exactly(1).of(sensor).powerUp();
+    }});
+
+    camera.powerOn();
   }
 
 }
