c8dae3183cf57ab2e76ab5696b26d55d0d2d19b1
Luke Tan <Luke@Lukes-MacBook-Air.local>
2020-10-21 21:37:19 +0800

    Pressing the shutter with the power on copies data from the sensor to the memory card

--- src/main/java/ic/doc/camera/Camera.java
+++ src/main/java/ic/doc/camera/Camera.java
@@ -3,20 +3,28 @@ package ic.doc.camera;
 public class Camera {
 
   private final Sensor sensor;
+  private final MemoryCard memoryCard;
+  private boolean isPowerOn;
 
-  public Camera(Sensor sensor) {
+  public Camera(Sensor sensor, MemoryCard memoryCard) {
     this.sensor = sensor;
+    this.memoryCard = memoryCard;
+    this.isPowerOn = false;
   }
 
   public void pressShutter() {
-    // not implemented
+    if (isPowerOn) {
+      memoryCard.write(sensor.readData());
+    }
   }
 
   public void powerOn() {
+    this.isPowerOn = true;
     sensor.powerUp();
   }
 
   public void powerOff() {
+    this.isPowerOn = false;
     sensor.powerDown();
   }
 }
--- src/test/java/ic/doc/camera/CameraTest.java
+++ src/test/java/ic/doc/camera/CameraTest.java
@@ -12,10 +12,11 @@ public class CameraTest {
   public JUnitRuleMockery context = new JUnitRuleMockery();
 
   private Sensor sensor = context.mock(Sensor.class);
+  private MemoryCard memoryCard = context.mock(MemoryCard.class);
 
   @Test
   public void switchingTheCameraOnPowersUpTheSensor() {
-    camera = new Camera(sensor);
+    camera = new Camera(sensor, memoryCard);
 
     context.checking(new Expectations() {{
       exactly(1).of(sensor).powerUp();
@@ -26,13 +27,29 @@ public class CameraTest {
 
   @Test
   public void switchingTheCameraOffPowersDownTheSensor() {
-    camera = new Camera(sensor);
+    camera = new Camera(sensor, memoryCard);
 
     context.checking(new Expectations() {{
       exactly(1).of(sensor).powerDown();
     }});
 
     camera.powerOff();
+  }
+
+  @Test
+  public void pressingTheShutterWithThePowerOnCopiesDataFromTheSensorToTheMemoryCard() {
+    camera = new Camera(sensor, memoryCard);
+    byte[] mockedSensorReadData = {1, 2, 3, 4};
+
+    context.checking(new Expectations() {{
+      ignoring(sensor).powerUp();
+      exactly(1).of(sensor).readData();
+      will(returnValue(mockedSensorReadData));
+      exactly(1).of(memoryCard).write(mockedSensorReadData);
+    }});
+
+    camera.powerOn();
+    camera.pressShutter();
   }
 
 }
